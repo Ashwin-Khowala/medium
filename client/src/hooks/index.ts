c88b5d8ae086a601f8f9512b2037039b2c5b2121
Ashwin-Khowala <ashwinkhowala1@gmail.com>
2025-03-14 10:57:39 +0530

    some chages

--- client/src/hooks/index.ts
+++ client/src/hooks/index.ts
@@ -1,10 +1,20 @@
 import { useEffect, useState } from "react";
-import axios, { Axios } from "axios";
+import axios from "axios";
 import {SERVER_URL} from "../config";
+
+
+interface Blog{
+    "content":string;
+    "title":string;
+    "id":string;
+    "author":{
+      "name":string;
+    }
+  }
 
 export const useBlogs=()=>{
     const [loading,setLoading]=useState(true);
-    const [blogs ,setBlogs]=useState([]);
+    const [blogs ,setBlogs]=useState<Blog[]>([]);
 
     useEffect(()=>{
         axios.get(`${SERVER_URL}/api/v1/blog/bulk`,{
